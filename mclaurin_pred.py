2e52bfcda3d7f8d652d65e638ceabe1b52281ad8
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-10-04 14:50:15 +0200

    Update mclaurin_pred.py

--- mclaurin_pred.py
+++ mclaurin_pred.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 test = np.zeros(200)
 x = -1
 for i in range(0,200):
-  test[i]= math.log(x+2)#Funzione da modificare durante i test
+  test[i]= math.sin(x)#Funzione da modificare durante i test
   x=round(x+0.01,2)
 #__________________________________________
 #Preparazione dati di input
@@ -26,10 +26,9 @@ print(pred)
 print("____________________________________________")
 print("CONFRONTO GRAFICO")
 t = np.arange(-1.0, 1.0, 0.01)
-y_true = np.log(t+2)#Funzione da modificare durante i test
 y_pred = pred[0][0]+ (pred[0][1]*t) + (pred[0][2]*pow(t,2)) + (pred[0][3]*pow(t,3))
 fig, ax = plt.subplots()
-ax.plot(t, y_true)
+ax.plot(t, test)
 ax.plot(t, y_pred)
 ax.grid()
 plt.legend(['y_true', 'y_pred'], loc='upper left')
